d670d54332b934e10792521e7ef213164871a92c
kijima <norimaking777@gmail.com>
2020-03-23 03:57:22 +0900

    コンセプトのサムネイル入れ替え

--- front-page.php
+++ front-page.php
@@ -91,13 +91,13 @@
 			<div class="container">
 				<!-- 上段 -->
 				<div class="row mb-4">
-					<div class="col-md-3 order-2">
-						<img src="<?php bloginfo('template_url'); ?>/images/about02.jpg" alt="店主が厳選したこだわりのコーヒー" class="img-fluid rounded" style="" />
-					</div>
-					<div class="col-md-9 mb-3 order-1">
+					<div class="col-md-9 mb-3 order-md-1">
 						<h3 class="mb-3">数学塾</h3>
 						<p>〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として…高校生…〜時から〜時は数学塾として</p>
 						<a href="#menu" class="btn btn-primary">数学塾について</a>
+					</div>
+					<div class="col-md-3 order-md-0">
+						<img src="<?php bloginfo('template_url'); ?>/images/about02.jpg" alt="店主が厳選したこだわりのコーヒー" class="img-fluid rounded" style="" />
 					</div>
 				</div>
 			</div>
